2f2606fdd71f8c296c9f3420b0d94a331a66f739
xyazzrany <jaxoncai3@gmail.com>
2023-04-24 18:33:59 +0800

    更新上传到阿里云盘

--- aliyun.py
+++ aliyun.py
@@ -4,4 +4,4 @@ from aligo import Auth
 
 ali = Aligo(name='refresh_token' , refresh_token='3a5ea54f522d4c0daa67ad039b526372')
 
-ali.upload_file('payload-dumper-go', parent_file_id='64460e3d011411c9632c46fdbba2e2651aab3cfa')   
+ali.upload_file('~/Magisk/done/*', parent_file_id='64460e3d011411c9632c46fdbba2e2651aab3cfa')   
